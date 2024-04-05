96d8d0d008d7aa2e37666d0325e9b32aeb6e947f
Driton Gashi <dritongashi1995@gmail.com>
2024-04-05 17:38:55 +0200

    Fixing Template bugs, converting Jquery to React code

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,45 +1,29 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 // CSS
-import "./css/main.css"
-import "./css/fontawesome-all.min.css"
-import "./jquery/myJquery";
+import "./css/main.css";
+import "./css/fontawesome-all.min.css";
 
 // pages & components
-import Home from './pages/Home'
-import Sidebar from './components/Sidebar'
-import Dashboard from './pages/Dashboard';
-import Elements from './pages/Elements';
-import Generic from './pages/Generic';
+import Home from "./pages/Home";
+import Sidebar from "./components/Sidebar";
+import Dashboard from "./pages/Dashboard";
+import Elements from "./pages/Elements";
+import Generic from "./pages/Generic";
 
 function App() {
-
   return (
     <>
       <BrowserRouter>
         <div id="wrapper">
-          <main id='main'>
-          <Routes>
-            <Route 
-              path="/" 
-              element={<Home />} 
-            />
-            <Route 
-              path="/dashboard" 
-              element={<Dashboard />} 
-            />
-            <Route 
-              path="/elements" 
-              element={<Elements />} 
-            />
-            <Route 
-              path="/generic" 
-              element={<Generic />} 
-            />
-          </Routes>
+          <main id="main">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/dashboard" element={<Dashboard />} />
+              <Route path="/elements" element={<Elements />} />
+              <Route path="/generic" element={<Generic />} />
+            </Routes>
           </main>
-        <Sidebar />
-          
-
+          <Sidebar />
         </div>
       </BrowserRouter>
     </>
@@ -47,4 +31,3 @@ function App() {
 }
 
 export default App;
-
--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,99 +1,117 @@
-import { useState } from 'react';
-import { Link } from 'react-router-dom'
-import Footer from "../components/Footer"
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import Footer from "../components/Footer";
 
 const Sidebar = () => {
-  const [sidebarActive, setSidebarActive] = useState(false);
+    const [sidebarActive, setSidebarActive] = useState(false);
 
-  const toggleSidebar = () => {
-    setSidebarActive(!sidebarActive);
-  };
-  return (
-    <div id="sidebar" className={sidebarActive ? '' : 'inactive'}>      
-        <div className="inner">
+    const toggleSidebar = () => {
+        setSidebarActive(!sidebarActive);
+    };
 
-  <section id="search" className="alt">
-    <form method="post" action="#">
-      <input type="text" name="query" id="query" placeholder="Search" />
-    </form>
-  </section>
+    const toggleMenu = (event) => {
+        event.preventDefault();
+        const target = event.target;
+        const opener = target.closest('.opener');
+        const menuOpeners = document.querySelectorAll('#menu .opener');
+        
+        menuOpeners.forEach(openerElement => {
+            if (openerElement !== opener) {
+                openerElement.classList.remove('active');
+            }
+        });
+        
+        if (opener) {
+            opener.classList.toggle('active');
+            // You can trigger any additional event handling here if needed
+        }
+    };
 
-  <nav id="menu">
-    <header className="major">
-      <h2>Menu</h2>
-    </header>
-    <ul>
-      <li><Link to="/">Homepage</Link></li>
-      <li><Link to="generic">Generic</Link></li>
-      <li><Link to="elements">Elements</Link></li>
-      <li>
-        <span className="opener">Submenu</span>
-        <ul>
-          <li><a href="#">Lorem Dolor</a></li>
-          <li><a href="#">Ipsum Adipiscing</a></li>
-          <li><a href="#">Tempus Magna</a></li>
-          <li><a href="#">Feugiat Veroeros</a></li>
-        </ul>
-      </li>
-      <li><a href="#">Etiam Dolore</a></li>
-      <li><a href="#">Adipiscing</a></li>
-      <li>
-        <span className="opener">Another Submenu</span>
-        <ul>
-          <li><a href="#">Lorem Dolor</a></li>
-          <li><a href="#">Ipsum Adipiscing</a></li>
-          <li><a href="#">Tempus Magna</a></li>
-          <li><a href="#">Feugiat Veroeros</a></li>
-        </ul>
-      </li>
-      <li><a href="#">Maximus Erat</a></li>
-      <li><a href="#">Sapien Mauris</a></li>
-      <li><a href="#">Amet Lacinia</a></li>
-    </ul>
-  </nav>
+    return (
+        <div id="sidebar" className={sidebarActive ? '' : 'inactive'}>
+            <div className="inner">
+                <section id="search" className="alt">
+                    <form method="post" action="#">
+                        <input type="text" name="query" id="query" placeholder="Search" />
+                    </form>
+                </section>
 
-  <section>
-    <header className="major">
-      <h2>Ante interdum</h2>
-    </header>
-    <div className="mini-posts">
-      <article>
-        <a href="#" className="image"><img src="images/pic07.jpg" alt="" /></a>
-        <p>Aenean ornare velit lacus, ac varius enim lorem ullamcorper dolore aliquam.</p>
-      </article>
-      <article>
-        <a href="#" className="image"><img src="images/pic08.jpg" alt="" /></a>
-        <p>Aenean ornare velit lacus, ac varius enim lorem ullamcorper dolore aliquam.</p>
-      </article>
-      <article>
-        <a href="#" className="image"><img src="images/pic09.jpg" alt="" /></a>
-        <p>Aenean ornare velit lacus, ac varius enim lorem ullamcorper dolore aliquam.</p>
-      </article>
-    </div>
-    <ul className="actions">
-      <li><a href="#" className="button">More</a></li>
-    </ul>
-  </section>
+                <nav id="menu">
+                    <header className="major">
+                        <h2>Menu</h2>
+                    </header>
+                    <ul>
+                        <li><Link to="/">Homepage</Link></li>
+                        <li><Link to="generic">Generic</Link></li>
+                        <li><Link to="elements">Elements</Link></li>
+                        <li>
+                            <span className="opener" onClick={toggleMenu}>Submenu</span>
+                            <ul>
+                                <li><a href="#">Lorem Dolor</a></li>
+                                <li><a href="#">Ipsum Adipiscing</a></li>
+                                <li><a href="#">Tempus Magna</a></li>
+                                <li><a href="#">Feugiat Veroeros</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="#">Etiam Dolore</a></li>
+                        <li><a href="#">Adipiscing</a></li>
+                        <li>
+                            <span className="opener" onClick={toggleMenu}>Another Submenu</span>
+                            <ul>
+                                <li><a href="#">Lorem Dolor</a></li>
+                                <li><a href="#">Ipsum Adipiscing</a></li>
+                                <li><a href="#">Tempus Magna</a></li>
+                                <li><a href="#">Feugiat Veroeros</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="#">Maximus Erat</a></li>
+                        <li><a href="#">Sapien Mauris</a></li>
+                        <li><a href="#">Amet Lacinia</a></li>
+                    </ul>
+                </nav>
 
-  <section>
-    <header className="major">
-      <h2>Get in touch</h2>
-    </header>
-    <p>Sed varius enim lorem ullamcorper dolore aliquam aenean ornare velit lacus, ac varius enim lorem ullamcorper dolore. Proin sed aliquam facilisis ante interdum. Sed nulla amet lorem feugiat tempus aliquam.</p>
-    <ul className="contact">
-      <li className="icon solid fa-envelope"><a href="#">information@untitled.tld</a></li>
-      <li className="icon solid fa-phone">(000) 000-0000</li>
-      <li className="icon solid fa-home">1234 Somewhere Road #8254<br />
-      Nashville, TN 00000-0000</li>
-    </ul>
-  </section>
+                <section>
+                    <header className="major">
+                        <h2>Ante interdum</h2>
+                    </header>
+                    <div class="mini-posts">
+										<article>
+											<a href="#" class="image"><img src="images/pic07.jpg" alt="" /></a>
+											<p>Aenean ornare velit lacus, ac varius enim lorem ullamcorper dolore aliquam.</p>
+										</article>
+										<article>
+											<a href="#" class="image"><img src="images/pic08.jpg" alt="" /></a>
+											<p>Aenean ornare velit lacus, ac varius enim lorem ullamcorper dolore aliquam.</p>
+										</article>
+										<article>
+											<a href="#" class="image"><img src="images/pic09.jpg" alt="" /></a>
+											<p>Aenean ornare velit lacus, ac varius enim lorem ullamcorper dolore aliquam.</p>
+										</article>
+									</div>
+                </section>
 
- <Footer/>
+                <section>
+                    <header className="major">
+                        <h2>Get in touch</h2>
+                    </header>
+                    <p>Sed varius enim lorem ullamcorper dolore aliquam aenean ornare velit lacus, ac varius enim lorem ullamcorper dolore. Proin sed aliquam facilisis ante interdum. Sed nulla amet lorem feugiat tempus aliquam.</p>
+									<ul class="contact">
+										<li class="icon solid fa-envelope"><a href="#">information@untitled.tld</a></li>
+										<li class="icon solid fa-phone">(000) 000-0000</li>
+										<li class="icon solid fa-home">1234 Somewhere Road #8254<br />
+										Nashville, TN 00000-0000</li>
+									</ul>
+                </section>
 
-</div>
-<Link to="#" className="toggle" onClick={toggleSidebar}/>
-    </div>
-  )
-}
+                <Footer />
+            </div>
 
-export default Sidebar
+            {/* Toggle link */}
+            <Link to="#" className="toggle" onClick={toggleSidebar}>
+                Toggle
+            </Link>
+        </div>
+    );
+};
+
+export default Sidebar;
